ac8ef649a7bfd95aeb939094f76094d5ece8cff5
Flxbot <92648520+FlxBot001@users.noreply.github.com>
2024-11-19 03:00:46 +0300

    Updated views file

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+   return render(request, 'home.html')
+
+def base(request):
+   return render(request, 'base.html')
+
+def add_product(request):
+   return render(request, 'add_product.html')
